fdae6d9a0b3c45d927044c5f3ee9df9b28df50bd
André de Paula Terceiro <andre.terceiro@cerc.com>
2024-02-19 17:50:06 -0300

    A code that do not work, but is a progess...

--- jankenpo-rust/src/main.rs
+++ jankenpo-rust/src/main.rs
@@ -1,4 +1,25 @@
+fn read_user_input() -> str {
+    use std::io::{stdin,stdout,Write};
+    let mut s=String::new();
+    print!("Please enter some text: ");
+    let _=stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    return s;
+}
+
 fn main() {
     println!("Jankenpo!!!\n");
     println!("Please type 'p' for 'paper', 'r' for 'rock' or 's' for 'scissors':");
+    let mut userChoice = "";
+    
+    while userChoice != "p" && userChoice != "r" && userChoice != "s" {
+        userChoice = read_user_input();
+        //println!("{}", userChoice);
+    }
 }
